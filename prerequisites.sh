aac2731acc0582a0d289562d93a739b2b1782ad4
jaehwanspin <jaehwanspin@gmail.com>
2020-01-07 09:32:06 +0900

    fucking

---
+++ prerequisites.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo -A apt-get update
+sudo -A apt-get install -y --no-install-recommends pkg-config
+sudo -A apt-get install -y --no-install-recommends autoconf
+sudo -A apt-get install -y --no-install-recommends libpulse-dev:amd64
+sudo -A apt-get install -y --no-install-recommends libgtk-3-dev:amd64
