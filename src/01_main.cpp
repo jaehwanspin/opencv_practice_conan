506b319ac1fe45df9644df53f4b3387c82ae923a
Jin <37390717+jaehwanspin@users.noreply.github.com>
2020-01-17 19:52:39 +0900

    Update 01_main.cpp

--- src/01_main.cpp
+++ src/01_main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <string>
 
-#include <opencv4/opencv2/core.hpp>
-#include <opencv4/opencv2/imgcodecs.hpp>
-#include <opencv4/opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <opencv2/core/ocl.hpp>
 
 int main(int argc, char** argv)
 {
-    std::cout << "5252" << std::endl;
+    cv::ocl::setUseOpenCL(true);
+    std::cout << "fucking hell" << std::endl;
 
     std::string img_name = argv[1];
-    cv::Mat img = cv::imread(img_name.c_str(), -1);
+    cv::UMat img = cv::imread(img_name.c_str(), -1).getUMat(cv::ACCESS_RW, cv::USAGE_DEFAULT);
     if (img.empty()) return -1;
     std::cout << "image read" << std::endl;
     cv::namedWindow("lena", cv::WINDOW_AUTOSIZE);
