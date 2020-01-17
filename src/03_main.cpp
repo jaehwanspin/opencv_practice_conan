40a54ee1bb3d9b535cd0e714ee00f062fcb50c85
Jin <37390717+jaehwanspin@users.noreply.github.com>
2020-01-17 19:53:31 +0900

    Update 03_main.cpp

--- src/03_main.cpp
+++ src/03_main.cpp
@@ -1,77 +1,55 @@
 #include <iostream>
-#include <fstream>
-#include <thread>
+#include <string>
+#include <array>
 
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/ocl.hpp>
 
-int g_slider_position = 0;
+#include "utils.hpp"
 
-int g_run = 1;
-int g_dont_set = 0;
+#define ONCE 0
 
-cv::VideoCapture g_video_capture;
-
-void on_trackbar_slide(int pos, void*)
+void example(const cv::UMat& image)
 {
-    if (!g_dont_set)
+    cv::namedWindow("in", cv::WINDOW_AUTOSIZE);
+    
+    cv::imshow("in", image);
+
+    cv::UMat out;
+    image.copyTo(out);
+#if   ONCE == 1
+    cv::GaussianBlur(image, out, cv::Size(5, 5), 2, 0);
+    cv::imshow("out", out);
+#else
+    const size_t frames_count = 500;
+    std::array<cv::UMat, frames_count> frames;
+
+    for (cv::UMat& frame : frames)
     {
-        g_run = 1;
-        g_video_capture.set(cv::CAP_PROP_POS_FRAMES, pos);
+        cv::GaussianBlur(out, frame, cv::Size(5, 5), 3, 3);
+        std::cout << "gaussian bloom processing" << std::endl;
+        frame.copyTo(out);
     }
-    g_dont_set = 0;
+    std::cout << "gaussian bloom processing done" << std::endl;
+    cv::namedWindow("out", cv::WINDOW_AUTOSIZE);
+    for (cv::UMat& frame : frames)
+    {
+        cv::Mat single_frame = frame.getMat(cv::AccessFlag::ACCESS_READ);
+        cv::imshow("out", single_frame);
+        //std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+#endif
+
+    cv::waitKey(0);
 }
 
 int main(int argc, char** argv)
 {
-    std::string video_name = std::string(argv[1]);
-    cv::namedWindow("lena", cv::WINDOW_AUTOSIZE);
-    g_video_capture.open(video_name);
+    if (!fucking::setGPU()) return -1;
+    std::string img_name = argv[1];
+    cv::UMat in = cv::imread(img_name, -1).getUMat(cv::ACCESS_RW, cv::USAGE_DEFAULT);
 
-    int frames = (int)g_video_capture.get(cv::CAP_PROP_FRAME_COUNT);
-    int temp_w = (int)g_video_capture.get(cv::CAP_PROP_FRAME_WIDTH);
-    int temp_h = (int)g_video_capture.get(cv::CAP_PROP_FRAME_HEIGHT);
-
-    std::cout << "video has " << frames << " frames of dimensions("
-              << temp_w << ", " << temp_h << ")." << std::endl;
-
-    cv::createTrackbar("position", "lena", &g_slider_position, frames, on_trackbar_slide);
-
-    cv::Mat frame;
-
-    while (true)
-    {
-        if (g_run != 0)
-        {
-            g_video_capture >> frame;
-            if (frame.empty()) break;
-            int current_pos = (int)g_video_capture.get(cv::CAP_PROP_POS_FRAMES);
-            g_dont_set = 1;
-
-            cv::setTrackbarPos("position", "lena", current_pos);
-            cv::imshow("lena", frame);
-
-            g_run = 1;
-        }
-
-        char c = (char)cv::waitKey(10);
-        if (c == 's')
-        {
-            g_run = 1;
-            std::cout << "single step, run = " << g_run << std::endl;
-        }
-        if (c == 'r')
-        {
-            g_run = -1;
-            std::cout << "run mode, run = " << g_run << std::endl;
-        }
-        if (c == 27)
-        {
-            break;            
-        }
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    }
+    example(in);
 
     return 0;
 }
