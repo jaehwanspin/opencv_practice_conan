ff54fb608a35dfcdd0af23cb03817dbc54097891
Jin <37390717+jaehwanspin@users.noreply.github.com>
2020-01-17 19:52:16 +0900

    Update 00_main.cpp

--- src/00_main.cpp
+++ src/00_main.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
 
-#include <etl/vector.h>
-#include <etl/iterator.h>
-
 int main(int argc, char** argv)
 {
-    etl::vector<int, 100> vec;
-    vec.push_back(10);
-    vec.push_back(20);
-    vec.push_back(30);
-
-    for (int i = 0; i < vec.size(); i++)
-        std::cout << vec.at(i) << std::endl;
-
+    
     return 0;
 }
