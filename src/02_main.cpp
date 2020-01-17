14cf851a7567a013bc13d133c40f7eddd206e0d5
Jin <37390717+jaehwanspin@users.noreply.github.com>
2020-01-17 19:52:57 +0900

    Update 02_main.cpp

--- src/02_main.cpp
+++ src/02_main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-#include <opencv4/opencv2/core.hpp>
-#include <opencv4/opencv2/imgcodecs.hpp>
-#include <opencv4/opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 int main(int argc, char** argv)
 {
