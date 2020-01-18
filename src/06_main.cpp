80d24b7aae33458245e646a69732d2bad2d51fb2
Jin <37390717+jaehwanspin@users.noreply.github.com>
2020-01-18 16:57:37 +0900

    Update 06_main.cpp

--- src/06_main.cpp
+++ src/06_main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 #include <string>
 
-#include <opencv4/opencv2/opencv.hpp>
-#include <opencv4/opencv2/core/ocl.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/ocl.hpp>
+
+#include "utils.hpp"
 
 int main(int argc, char** argv)
 {
+    std::cout << "program starts" << std::endl;
+    if (!fucking::setGPU()) return -1;
+    cv::Mat lena = cv::imread(argv[1], cv::IMREAD_COLOR);
 
+    // create a matrix for split
+    cv::Mat bgr[3];
 
+    // split image into 3 single-channel matrices
+    cv::split(lena, bgr);
+
+    // create 4 windows
+    cv::namedWindow("Original Image");
+    cv::namedWindow("Red Channel");
+    cv::namedWindow("Green Channel");
+    cv::namedWindow("Blue Channel");
+
+    // show 4 windows
+    cv::imshow("Original Image", lena);
+    cv::imshow("Red Channel", bgr[2]);  // caution : the red channel index is 2!
+    cv::imshow("Green Channel", bgr[1]);
+    cv::imshow("Blue Channel", bgr[0]);
+
+    cv::waitKey(0);
     return 0;
 }
