246ddda515616019cd2c9bdc12e53de222ea0404
jaehwanspin <jaehwanspin@gmail.com>
2020-01-19 01:06:59 +0900

    fdsa

--- prerequisites.sh
+++ prerequisites.sh
@@ -5,3 +5,5 @@ sudo -A apt-get install -y --no-install-recommends pkg-config
 sudo -A apt-get install -y --no-install-recommends autoconf
 sudo -A apt-get install -y --no-install-recommends libpulse-dev:amd64
 sudo -A apt-get install -y --no-install-recommends libgtk-3-dev:amd64
+
+pip3 install opencv-python
