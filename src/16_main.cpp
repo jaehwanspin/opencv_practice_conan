4128a27dbdb671c658dfb8a21ed81b3056ff8bf9
unknown <jaehwanspin@gmail.com>
2020-02-08 19:53:07 +0900

    qwer

---
+++ src/16_main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+int main(size_t argc, char** argv)
+{
+	
+
+	return 0;
+}
